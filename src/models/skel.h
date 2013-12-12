4fd383171377d7e4db3924f7611336975d80f7cf
Jean-Benoist Leger <leger@jblg.fr>
2013-12-12 18:16:43 +0100

    skel

--- src/models/skel.h
+++ src/models/skel.h
@@ -45,7 +45,8 @@ class MODEL
     };
 
     /* Keep this as is. This will be usefull when symmetric SBM will be
-     * implemented */
+     * implemented
+     */
     struct is_sbm_symmetric
     {
         enum { value=false };
@@ -60,14 +61,16 @@ class MODEL
     {
         /* Here you must intialize the number of parameters (n_parameters) and
          * your model parameters, knowing the membership and the network in the
-         * SBM case */
+         * SBM case
+         */
     }
     
     MODEL(LBM & membership, MODEL::network & net)
     {
         /* Here you must intialize the number of parameters (n_parameters) and
          * your model parameters, knowing the membership and the network in the
-         * LBM case */
+         * LBM case 
+         */
     }
 
     inline
@@ -90,7 +93,7 @@ class MODEL
         /* Here you should provide a method to compute the prediction of the
          * network knowing parameters and the membership in the case of SBM. The
          * network object is provided, but only covariate fields shoud be used
-         * */
+         */
 
         /* The result is a list for R, as the same organization than the
          * provided network by R. See the constructor of MODEL::network you have
@@ -107,7 +110,7 @@ class MODEL
         /* Here you should provide a method to compute the prediction of the
          * network knowing parameters and the membership in the case of SBM. The
          * network object is provided, but only covariate fields shoud be used
-         * */
+         */
 
         /* The result is a list for R, as the same organization than the
          * provided network by R. See the constructor of MODEL::network you have
@@ -116,7 +119,16 @@ class MODEL
         
         return Lpred;
     }
+
+    /* Keep this. When this is not usefull, this is not used. */
+    inline vec to_vector();
+    MODEL(SBM &, const vec &);
+    MODEL(LBM &, const vec &);
 };
+
+
+
+
 
 /* Function for the model.
  */
@@ -143,7 +155,9 @@ class MODEL
 
 
 
-/* If you have direct formula to compute the following form, use it, else skip
+/* Usefull, Optional
+ *
+ * If you have direct formula to compute the following form, use it, else skip
  * this definition and delete this definition.
  *
  * forall i,q ; lZ_{iq} += \sum_{jl} Z_{jl} logf(i,j,q,l)
@@ -164,7 +178,9 @@ class MODEL
 
 
 
-/* If you have direct formula to compute the following form, use it, else skip
+/* Usefull, Optional
+ *
+ * If you have direct formula to compute the following form, use it, else skip
  * this definition and delete this definition.
  *
  * forall i,q ; lZ1_{iq} += \sum_{jl} Z2_{jl} logf(i,j,q,l)
@@ -187,19 +203,22 @@ class MODEL
 
 
 
-/* If you have a explicit formula to compute the maximum of PL in respect to
+/* Usefull, Optional
+ *
+ * If you have a explicit formula to compute the maximum of PL in respect to
  * model parameters, knowing the membership, use it, else skip and delete this
  * definition. In the case of SBM.
  *
- * This function must return PL in the maximum. You can use the function PL if
- * you have define one. If you have not define one. A generic function is used,
- * but it is unprobable you have a direct way to compute the maximum of PL
- * without having a direct way to compute PL.
+ * This function must return PL in the maximum.
+ * 
+ * This is usefull for SBM. See bernoulli and poisson for example.
  */
 
 // template<>
 // inline
-// double m_step(SBM & membership, poisson & model, poisson::network & net)
+// double m_step(SBM & membership,
+//               MODEL & model,
+//               MODEL::network & net)
 // {
 // }
 
@@ -207,32 +226,290 @@ class MODEL
 
 
 
-template<>
-inline
-double m_step(LBM & membership, poisson & model, poisson::network & net)
-{
-    model.lambda = (membership.Z1.t() * net.adj * membership.Z2)
-                    /
-                   (membership.Z1.t() * net.Mones * membership.Z2);
+/* Usefull, Optional
+ *
+ * If you have a explicit formula to compute the maximum of PL in respect to
+ * model parameters, knowing the membership, use it, else skip and delete this
+ * definition. In the case of LBM.
+ *
+ * This function must return PL in the maximum.
+ * 
+ * This is usefull for LBM. See bernoulli and poisson for example.
+ */
 
-    return
-        (
-            accu(
-                
-                    (
-                        model.lambda 
-                        % 
-                        (membership.Z1.t() * net.Mones * membership.Z2)
-                    )
-                    +
-                    (
-                        log(model.lambda)
-                        %
-                        (membership.Z1.t() * net.adj * membership.Z2)
-                    )
-                )
-            + net.accu_log_fact_X
-        );
-}
+// template<>
+// inline
+// double m_step(LBM & membership,
+//               MODEL & model,
+//               MODEL::network & net)
+// {
+// }
 
 
+
+
+
+/* If you have defined m_step(SBM &, MODEL &, MODEL::network &)
+ * Then
+ *     Useless
+ * Else
+ *     Usefull, Optional
+ *
+ * If you have direct formula to compute PL, without loops over (i,j,q,l), in
+ * the case of SBM, use it.
+ *
+ * See poisson_covariate for example.
+ */
+
+// template<>
+// inline
+// double PL(MODEL & model,
+//           SBM & membership,
+//           MODEL::network & net)
+
+
+
+
+
+/* If you have defined m_step(LBM &, MODEL &, MODEL::network &)
+ * Then
+ *     Useless
+ * Else
+ *     Usefull, Optional
+ *
+ * If you have direct formula to compute PL, without loops over (i,j,q,l), in
+ * the case of LBM, use it.
+ *
+ * See poisson_covariate for example.
+ */
+
+// template<>
+// inline
+// double PL(MODEL & model,
+//           LBM & membership,
+//           MODEL::network & net)
+
+
+
+
+
+/* If you have defined m_step(SBM &, MODEL &, MODEL::network &)
+ * Then
+ *     Useless
+ * Else
+ *     Usefull, Optional
+ *
+ * If you have a direct formula to compute the gradient of PL in respect to your
+ * parameters vectors without loops over (i,j,q,l), in the case of SBM, use it.
+ *
+ * See poisson_covariates for example.
+ */
+
+// template<>
+// inline
+// vec grad(MODEL & model,
+//          SBM & membership,
+//          MODEL::network & net)
+// {
+// }
+
+
+
+
+
+/* If you have defined m_step(LBM &, MODEL &, MODEL::network &)
+ * Then
+ *     Useless
+ * Else
+ *     Usefull, Optional
+ *
+ * If you have a direct formula to compute the gradient of PL in respect to your
+ * parameters vectors without loops over (i,j,q,l), in the case of LBM, use it.
+ *
+ * See poisson_covariates for example.
+ */
+
+// template<>
+// inline
+// vec grad(MODEL & model,
+//          SBM & membership,
+//          MODEL::network & net)
+// {
+// }
+
+
+
+
+
+/* If one of the following specialization
+ *     - grad(MODEL &, LBM &, MODEL::network)
+ *     - grad(MODEL &, SBM &, MODEL::network)
+ *   is Usefull and not defined
+ * Then
+ *     Usefull, Optional
+ * Else
+ *     Useless
+ *
+ * The gradient of logf(i,j,q,l) in respect to the parameter vector.
+ */
+
+// template<>
+// inline
+// vec grad_logf(MODEL & model,
+//               MODEL::network & net,
+//               unsigned int i,
+//               unsigned int j,
+//               unsigned int q,
+//               unsigned int l)
+// {
+// }
+
+
+
+
+
+/* If grad(MODEL &,
+ *         MODEL::network &,
+ *         unsigned int,
+ *         unsigned int,
+ *         unsigned int,
+ *         unsigned int)
+ *    is Usefull and not defined
+ * Then
+ *     Usefull, Mandatory
+ * Else
+ *     Useless
+ *
+ *
+ * The derivative of logf(i,j,q,l) in respect to the k-th parameters of the
+ * parameter vector.
+ *
+ * See naive_bernoulli for example.
+ */
+
+// inline
+// double grad_logf(MODEL & model,
+//                  MODEL::network & net,
+//                  unsigned int i,
+//                  unsigned int j,
+//                  unsigned int q,
+//                  unsigned int l,
+//                  unsigned int k)
+// {
+// }
+
+
+
+
+
+/* If one of the following specialization
+ *     - PL(MODEL &, SBM &, MODEL::network)
+ *     - PL(MODEL &, LBM &, MODEL::network)
+ *   is Usefull and not definied
+ * Then
+ *     Usefull, Mandatory
+ * Else
+ *     Useless
+ *
+ * logf(i,j,q,l) see notations.
+ */
+
+// inline
+// double logf(MODEL & model,
+//             MODEL::network & net,
+//             unsigned int i,
+//             unsigned int j,
+//             unsigned int q,
+//             unsigned int l)
+// {
+// }
+
+
+
+
+
+/* If one of the following specialization
+ *     - m_step(LBM &, MODEL &, MODEL::network)
+ *     - m_step(SBM &, MODEL &, MODEL::network)
+ *   is not defined
+ * Then
+ *     Usefull, Mandatory
+ * Else
+ *     Useless
+ *
+ * From the given model, in the given direction for model parameters, return the
+ * maximum betwenn 1 and the maximum step the descend algorithm can do.
+ *
+ * See naive_bernoulli for example.
+ *
+ * If you don't have any constraint on you parameter vector, return 1, else, use
+ * your brain.
+ */
+
+// inline
+// double maximum_step_in_direction(MODEL & model, vec direction)
+// {
+// }
+
+
+
+
+
+/* If one of the following specialization
+ *     - m_step(LBM &, MODEL &, MODEL::network)
+ *     - m_step(SBM &, MODEL &, MODEL::network)
+ *   is not defined
+ * Then
+ *     Usefull, Mandatory
+ * Else
+ *     Useless
+ *
+ * Export the model parameters as a vector.
+ *
+ * See naive_bernoulli for example.
+ */
+
+// inline
+// vec MODEL::to_vector()
+// {
+// }
+
+
+
+
+
+/* If m_step(SBM &, MODEL &, MODEL::network) is not defined
+ * Then
+ *     Usefull, Mandatory
+ * Else
+ *     Useless
+ * 
+ * Constructor of the model, from a vector of parameter named vectorized with
+ * the given membership of type SBM.
+ *
+ * See naive_bernoulli for example.
+ */
+
+// MODEL::MODEL(SBM & membership, const vec & vectorized)
+// {
+// }
+
+
+
+
+
+/* If m_step(LBM &, MODEL &, MODEL::network) is not defined
+ * Then
+ *     Usefull, Mandatory
+ * Else
+ *     Useless
+ * 
+ * Constructor of the model, from a vector of parameter named vectorized with
+ * the given membership of type LBM.
+ *
+ * See naive_bernoulli for example.
+ */
+
+// MODEL::MODEL(LBM & membership, const vec & vectorized)
+// {
+// }
