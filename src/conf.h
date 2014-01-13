f975df7a53ac9928473dc6dfd243f599ac50aa25
Jean-Benoist Leger <leger@jblg.fr>
2014-01-13 15:37:03 +0100

    configuration values

--- src/conf.h
+++ src/conf.h
@@ -1,9 +1,9 @@
 
 #define TOL_P 1e-1
-#define TOL_M 1e-5
+#define TOL_M 1e-4
 #define TOL_F 1e-1
-#define TOL_EM 1e-6
-#define BFGS_ITER_MAX 1000
+#define TOL_EM 1e-5
+#define BFGS_ITER_MAX 500
 #define LINE_SEARCH_ITER_MAX 40
 
 //#define DEBUG_EM
